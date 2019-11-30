1ded476e4f4640ae787ea2365487df217e4d4470
andreabari <andreabari>
2019-11-30 21:24:55 +0000

    Start framework for sightings view

--- website/map/views.py
+++ website/map/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Squirrel
+
+
+def map(request):
+    return HttpResponse('There should be a map here')
+
+def sightings(request):
+    sightings_list = Squirrel.objects.order_by('-date')
+    template = loader.get_template('map/sightings.html')
+    context = {
+        'sightings_list':sightings_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
